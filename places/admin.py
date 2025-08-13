f2160a74289d8014ca88c98e12042771b8d52dbc
Katerina <e.mayakovskaya@gmail.com>
2025-08-13 12:29:13 +0300

    Add Place model to admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Place
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description_short')
+    search_fields = ('title',)
